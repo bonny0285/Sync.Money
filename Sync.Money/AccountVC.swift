0509df447a64f724e23f3a1d710cdb406fa36d89
Massimiliano <massimiliano@MacBook-Pro-di-Massimiliano.local>
2019-05-10 21:05:33 +0100

    new layout

--- Sync.Money/AccountVC.swift
+++ Sync.Money/AccountVC.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AccountVC: UIViewController {
+    
+    @IBOutlet weak var cardBtn: UIButton!
+    @IBOutlet weak var plusBtn: UIButton!
+    @IBOutlet weak var marketBtn: UIButton!
+    @IBOutlet weak var moreBtn: UIButton!
+    @IBOutlet weak var accountBtn: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
